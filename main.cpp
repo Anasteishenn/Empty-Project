a1ca0370174808d6f2f79b9b49dae1243a74180f
Anasteishenn <anastasisuvorova1@gmail.com>
2023-04-12 17:29:51 +0300

    Add main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include "main.h"
+#include "point.h"
 
 int main() {
+	Point p1;
+	Point p2;
 
+	cout << p1.info() << endl;
+	cout << p2.info() << endl;
+
+
+
+	/*int n = 10;
+	int m = 20;
+	int result = n + m;*/
+
+	/*string s1 = "Max";
+	string s2 = " is fool!";
+	string s = s1 + s2;*/
 	return 0;
 }
